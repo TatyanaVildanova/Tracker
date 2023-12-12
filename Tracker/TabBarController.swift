45de4ef86f04b7fa1ab5207626608b1060d2ef1c
Tatyana Vildanova <tz29@yandex.ru>
2023-12-12 16:48:00 +0300

    commit1

--- Tracker/TabBarController.swift
+++ Tracker/TabBarController.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.isTranslucent = false
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = .blue
+        
+        let trackersViewController = TrackersViewController()
+        let statisticsViewController = StatisticsViewController()
+        
+        trackersViewController.tabBarItem = UITabBarItem(
+            title: "Трекеры",
+            image: UIImage(named: "TrackerBar"),
+            selectedImage: nil
+        )
+        statisticsViewController.tabBarItem = UITabBarItem(
+            title: "Статистика",
+            image: UIImage(named: "StatisticBar"),
+            selectedImage: nil
+        )
+        
+        let controllers = [trackersViewController, statisticsViewController]
+        
+        viewControllers = controllers
+    }
+}
